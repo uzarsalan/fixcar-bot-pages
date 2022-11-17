dcbe71ebd49f636c7336c742e341f8bf3d079f15
Arsalan Rabdanov <uzarsalan@gmail.com>
2022-11-17 17:28:35 +0300

    added action to call button

--- src/routes/answers/[postId]/+page.svelte
+++ src/routes/answers/[postId]/+page.svelte
@@ -29,12 +29,13 @@
           >{statuses[data[key].availability]}</span
         >
       </p>
-      <a
-        href={`tel://${data[key].Market.phoneNumber}`}
+      <button
+        on:click={() =>
+          window.Telegram.openLink(`tel://${data[key].Market.phoneNumber}`)}
         class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >
         Позвонить
-      </a>
+      </button>
     </div>
   {/each}
 </div>
