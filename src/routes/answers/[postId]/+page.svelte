cf4d61c1093d3bff01f4b32e86bfdb36b6379763
Arsalan Rabdanov <uzarsalan@gmail.com>
2022-11-18 17:36:48 +0300

    no answers layout fix

--- src/routes/answers/[postId]/+page.svelte
+++ src/routes/answers/[postId]/+page.svelte
@@ -8,36 +8,44 @@
 </script>
 
 <div>
-  {#each Object.keys(data) as key}
-    <div
-      class="mb-3 p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700"
-    >
-      <a href="#">
-        <h5
-          class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
-        >
-          {data[key].Market.name}
-        </h5>
-      </a>
-      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-        {data[key].message}
-      </p>
-      <p class="mb-3 font-bold text-gray-700 dark:text-white">
-        {data[key].price + " руб"}
-        <span
-          class="p-1 bg-white border border-gray-200 rounded-lg dark:bg-green-800 dark:border-green-700"
-          >{statuses[data[key].availability]}</span
-        >
-      </p>
-      <button
-        on:click={() =>
-          window.Telegram.WebApp.openLink(
-            `https://${window.location.hostname}/callto/${data[key].Market.phoneNumber}`
-          )}
-        class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      >
-        Позвонить
-      </button>
+  {#if Object.keys(data).length == 0}
+    <div class="flex h-screen">
+      <div class="m-auto">
+        <h5>По этой заявке пока нет ответов 😔</h5>
+      </div>
     </div>
-  {/each}
+  {:else}
+    {#each Object.keys(data) as key}
+      <div
+        class="mb-3 p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700"
+      >
+        <a href="#">
+          <h5
+            class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+          >
+            {data[key].Market.name}
+          </h5>
+        </a>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          {data[key].message}
+        </p>
+        <p class="mb-3 font-bold text-gray-700 dark:text-white">
+          {data[key].price + " руб"}
+          <span
+            class="p-1 bg-white border border-gray-200 rounded-lg dark:bg-green-800 dark:border-green-700"
+            >{statuses[data[key].availability]}</span
+          >
+        </p>
+        <button
+          on:click={() =>
+            window.Telegram.WebApp.openLink(
+              `https://${window.location.hostname}/callto/${data[key].Market.phoneNumber}`
+            )}
+          class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Позвонить
+        </button>
+      </div>
+    {/each}
+  {/if}
 </div>
