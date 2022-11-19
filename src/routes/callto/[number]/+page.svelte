89424e4a4fb8e7314bacf39de0f77dad818c7139
Arsalan Rabdanov <uzarsalan@gmail.com>
2022-11-19 17:19:14 +0300

    fixed callto button

--- src/routes/callto/[number]/+page.svelte
+++ src/routes/callto/[number]/+page.svelte
@@ -2,6 +2,14 @@
   export let data;
 </script>
 
-<h1 on:click={() => (window.location.href = `tel://${data.number}`)}>
-  {data.number}
-</h1>
+<div class="flex h-screen">
+  <div class="m-auto">
+    <a href={`tel://${data.number}`}>
+      <button
+        class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        {data.number}
+      </button>
+    </a>
+  </div>
+</div>
