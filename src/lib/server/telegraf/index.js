c6b4c8d5cb506d1949af86df400a08ec77269e0e
Arsalan Rabdanov <uzarsalan@gmail.com>
2022-11-22 15:20:40 +0300

    disabled bot launch

--- src/lib/server/telegraf/index.js
+++ src/lib/server/telegraf/index.js
@@ -1,7 +1,7 @@
 import { Telegraf } from "telegraf";
 
 export const bot = new Telegraf(process.env.BOT_TOKEN || "");
-bot.launch();
+// bot.launch(); 
 
 // Enable graceful stop
 process.once("SIGINT", () => bot.stop("SIGINT"));
